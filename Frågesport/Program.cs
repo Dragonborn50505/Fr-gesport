e086457d961b49b7299f2816cc705565de5c806f
Vincent Allinggård <allinggrd.vincent@elev.ga.ntig.se>
2021-09-21 11:07:05 +0200

    working_version

--- Frågesport/Program.cs
+++ Frågesport/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
 
-sting name ="100"
+//string name = "100";
+//int points = 100;
+//points = +1;
+//Console.WriteLine(name + name);
+//Console.WriteLine(points + points);
+//Console.ReadLine();
 
-int points =100;
-int hp;
-int xp;
+int points = 0;
+string choice;
 
-console.writeLine(name + name);
-console.writeLine(points + points);
+Console.WriteLine("Fråga1");
+choice = Console.ReadLine();
 
-console.readLine();
+choice = choice.ToLower();
+
+if (choice == "a")
+points += 1;
+
+if (choice == "b")
+points += 0;
+
+if (choice == "c")
+points += 0;
+
+
+
+Console.WriteLine("Fråga2");
+choice = Console.ReadLine();
+
+if (choice == "a")
+points += 0;
+
+if (choice == "b")
+points += 1;
+
+if (choice == "c")
+points += 0;
+
+
+
+Console.WriteLine("Fråga3");
+choice = Console.ReadLine();
+
+if (choice == "a")
+points += 0;
+
+if (choice == "b")
+points += 0;
+
+if (choice == "c")
+points += 1;
+
+Console.WriteLine("Poäng");
+Console.WriteLine(points);
+choice = Console.ReadLine();
