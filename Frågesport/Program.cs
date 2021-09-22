823763f9274dc3759c7439fe314a9c4e0ac34d7f
Vincent Allinggård <allinggrd.vincent@elev.ga.ntig.se>
2021-09-22 11:05:13 +0200

    working

--- Frågesport/Program.cs
+++ Frågesport/Program.cs
@@ -9,54 +9,61 @@
 //while
 
 int points = 0;
-string choice;
-
-choice = choice.ToLower();
+string choice = "";
 
 
-while (choice == "a" || choice == "b" || choice == "c"){
-Console.WriteLine("Fråga1");
-choice = Console.ReadLine();
+while (choice != "a" && choice != "b" && choice != "c")
+{
+    Console.WriteLine("Fråga1");
+    choice = Console.ReadLine();
+    choice = choice.ToLower();
 
 
-if (choice == "a")
-points += 1;
+    if (choice == "a")
+        points += 1;
 
-if (choice == "b")
-points += 0;
+    if (choice == "b")
+        points += 0;
 
-if (choice == "c")
-points += 0;
+    if (choice == "c")
+        points += 0;
 }
 
-while (choice == "a" || choice == "b" || choice == "c"){
-Console.WriteLine("Fråga2");
-choice = Console.ReadLine();
+choice = "";
+while (choice != "a" && choice != "b" && choice != "c")
+{
+    Console.WriteLine("Fråga2");
+    choice = Console.ReadLine();
+    choice = choice.ToLower();
 
-if (choice == "a")
-points += 0;
+    if (choice == "a")
+        points += 0;
 
-if (choice == "b")
-points += 1;
+    if (choice == "b")
+        points += 1;
 
-if (choice == "c")
-points += 0;
+    if (choice == "c")
+        points += 0;
 }
+choice = "";
+while (choice != "a" && choice != "b" && choice != "c")
+{
+    Console.WriteLine("Fråga3");
+    choice = Console.ReadLine();
+    choice = choice.ToLower();
 
-while (choice == "a" || choice == "b" || choice == "c"){
-Console.WriteLine("Fråga3");
-choice = Console.ReadLine();
+    if (choice == "a")
+        points += 0;
 
-if (choice == "a")
-points += 0;
+    if (choice == "b")
+        points += 0;
 
-if (choice == "b")
-points += 0;
-
-if (choice == "c")
-points += 1;
+    if (choice == "c")
+        points += 1;
 }
 
 Console.WriteLine("Poäng");
 Console.WriteLine(points);
+
+
 choice = Console.ReadLine();
