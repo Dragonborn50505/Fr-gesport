29005e3e61b65709ad749787e9cc116d6bb25b96
Vincent Allinggård <allinggrd.vincent@elev.ga.ntig.se>
2021-09-29 11:08:02 +0200

    Strukturera_med_metoder

--- Frågesport/Program.cs
+++ Frågesport/Program.cs
@@ -10,41 +10,38 @@
 
 int points = 0;
 string choice = "";
+points += Question_1();
 
 
-while (choice != "a" && choice != "b" && choice != "c")
-{
-    Console.WriteLine("Fråga1");
-    choice = Console.ReadLine();
-    choice = choice.ToLower();
+Console.WriteLine("Fråga1");
+choice = GetAnswer();
+
+            static int Question_1()
+            {   
+            string choice = GetAnswer();
+            int points = 0;
+            if (choice == "a")
+            points += 1;
+            return points;
+            }
 
 
-    if (choice == "a")
-        points += 1;
 
-    if (choice == "b")
-        points += 0;
 
-    if (choice == "c")
-        points += 0;
-}
+Console.WriteLine("Fråga2");
+choice = GetAnswer();
 
-choice = "";
-while (choice != "a" && choice != "b" && choice != "c")
-{
-    Console.WriteLine("Fråga2");
-    choice = Console.ReadLine();
-    choice = choice.ToLower();
+ static int Question_2()
+            {   
+            string choice = GetAnswer();
+            int points = 0;
+            if (choice == "b")
+            points += 1;
+            return points;
+            }
 
-    if (choice == "a")
-        points += 0;
 
-    if (choice == "b")
-        points += 1;
 
-    if (choice == "c")
-        points += 0;
-}
 choice = "";
 while (choice != "a" && choice != "b" && choice != "c")
 {
@@ -62,8 +59,21 @@ while (choice != "a" && choice != "b" && choice != "c")
         points += 1;
 }
 
+
+
+
 Console.WriteLine("Poäng");
 Console.WriteLine(points);
 
 
 choice = Console.ReadLine();
+
+
+
+
+  static string GetAnswer()
+        {   string choice = "";
+        while (choice != "a" && choice != "b" && choice != "c")
+        {   choice = Console.ReadLine();
+        choice = choice.ToLower();
+        }return choice;}
